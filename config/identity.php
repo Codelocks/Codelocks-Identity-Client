26f9ff85291d4e56d6466d15ec873d9f6255d609
Peter Fan <peter.fan@codelocks.hk>
2023-06-12 15:37:57 +0800

    fixed config name

--- config/identity.php
+++ config/identity.php
@@ -7,11 +7,11 @@ return [
     'redirect'      => env('CODELOCKS_OAUTH_REDIRECT', env('APP_URL') . '/auth/callback'),
     'home'          => env('CODELOCKS_CLIENT_HOME', env('APP_URL') . '/home'),
 
-    'host'          => env('CODELOCKS_OAUTH_HOST'),
-    'authorize_url' => env('CODELOCKS_AUTHORIZE_HOST', '/oauth/authorize'),
-    'token_url'     => env('CODELOCKS_TOKEN_HOST', '/oauth/token'),
-    'refresh_url'   => env('CODELOCKS_REFRESH_TOKEN_HOST', '/oauth/token/refresh'),
-    'user_url'      => env('CODELOCKS_TOKEN_HOST', '/api/user/profile'),
+    'host'          => env('CODELOCKS_OAUTH_HOST', 'https://my.codelocksconnect.net'),
+    'authorize_url' => env('CODELOCKS_AUTHORIZE_URL', '/oauth/authorize'),
+    'token_url'     => env('CODELOCKS_TOKEN_URL', '/oauth/token'),
+    'refresh_url'   => env('CODELOCKS_REFRESH_TOKEN_URL', '/oauth/token/refresh'),
+    'user_url'      => env('CODELOCKS_TOKEN_URL', '/api/user/profile'),
     'scopes'        => env('CODELOCKS_TOKEN_SCOPES', 'profile profile.current_team'),
 
     'input_key' => 'token',
@@ -19,5 +19,5 @@ return [
         'redirect' => '/auth/redirect',
         'callback' => '/auth/callback'
     ],
-    'cookie' => 'codelocks_cookie'
+    'cookie'    => 'codelocks_cookie'
 ];
