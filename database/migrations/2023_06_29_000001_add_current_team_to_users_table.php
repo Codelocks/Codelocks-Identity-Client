22c9cd0a60e5ec09eefb4a4ff03a4b05308f44b4
Peter Fan <peter.fan@codelocks.hk>
2023-07-03 17:17:39 +0800

    add current_team field

---
+++ database/migrations/2023_06_29_000001_add_current_team_to_users_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration {
+    public function getConnection()
+    {
+        return config('database.portal');
+    }
+
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('users', function (Blueprint $table) {
+            if (!Schema::hasColumn('users', 'current_team')) {
+                $table->text('current_team');
+            }
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        if (Schema::hasColumn('users', 'current_team') ) {
+            Schema::dropColumns('users', 'current_team');
+        }
+    }
+};
